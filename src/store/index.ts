a5e429f4227b598afa228bd1fac4d0785ce95be5
Jon Higgins <jon.higgins@fictioneers.co.uk>
2022-06-07 09:40:27 +0100

    Hard code user_id

--- src/store/index.ts
+++ src/store/index.ts
@@ -20,7 +20,8 @@ export default createStore<State>({
   actions: {
     getCurrentUser: async ({ commit }) => {
       const tokenRequest = await axiosAdmin.post("/auth/token", {
-        user_id: `${Math.floor(Math.random() * 100000)}`,
+        user_id: "user_id",
+        // user_id: `${Math.floor(Math.random() * 100000)}`,
       });
       const token = tokenRequest.data.access_token;
 
@@ -29,14 +30,20 @@ export default createStore<State>({
         headers: { Authorization: `Bearer ${token}` },
       });
 
-      const createUserRequest = await axiosExperience.post("/users", {
-        published_timeline_id: process.env.VUE_APP_TIMELINE_ID,
-        timezone: "Europe/London",
-        disable_time_guards: false,
-        pause_at_beats: true,
-      });
+      let currentUserRequest;
 
-      commit("setCurrentUser", createUserRequest?.data?.data);
+      try {
+        currentUserRequest = await axiosExperience.get("/users/me");
+      } catch {
+        currentUserRequest = await axiosExperience.post("/users", {
+          published_timeline_id: process.env.VUE_APP_TIMELINE_ID,
+          timezone: "Europe/London",
+          disable_time_guards: false,
+          pause_at_beats: true,
+        });
+      }
+
+      commit("setCurrentUser", currentUserRequest?.data?.data);
     },
   },
   modules: {},
